454d11fce9c063934a335ea950675cb67b2bd5c2
Arjuna Panji <arjuna.panji97@gmail.com>
2022-12-03 15:07:54 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/register', [UserController::class, 'register'])->name('user.register');
-Route::get('/login', [UserController::class, 'login'])->name('user.login');
+Route::get('/', [UserController::class, 'login'])->name('user.login');
 Route::get('/logout', [UserController::class, 'logout'])->name('user.logout');
 Route::get('/forgot', [UserController::class, 'forgot_password'])->name('user.forgot');
 Route::post('/forgot', [UserController::class, 'reset_password'])->name('user.reset');
