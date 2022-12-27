37181349e3d070781c4c09a3dd05dbcc49c51d0a
Arjuna Panji <arjuna.panji97@gmail.com>
2022-12-27 17:43:23 +0700

    fix scanner

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -5,34 +5,13 @@
 <script>
     function onScanSuccess(decodedText, decodedResult) {
         // Handle on success condition with the decoded text or result.
-        console.log(`Scan result: ${decodedText}`, decodedResult);
+        alert(`Scan result: ${decodedText}`, decodedResult);
     }
 
     var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
         fps: 10,
         qrbox: 250,
     });
-    html5QrcodeScanner.render(onScanSuccess);
-
-    var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
-        fps: 10,
-        qrbox: 250,
-    });
-
-    function onScanSuccess(decodedText, decodedResult) {
-        // Handle on success condition with the decoded text or result.
-        console.log(`Scan result: ${decodedText}`, decodedResult);
-        // ...
-        html5QrcodeScanner.clear();
-        // ^ this will stop the scanner (video feed) and clear the scan area.
-    }
-
-    html5QrcodeScanner.render(onScanSuccess);
-
-    function onScanSuccess(decodedText, decodedResult) {
-        // Handle on success condition with the decoded text or result.
-        console.log(`Scan result: ${decodedText}`, decodedResult);
-    }
 
     function onScanError(errorMessage) {
         // handle on error condition, with error message
