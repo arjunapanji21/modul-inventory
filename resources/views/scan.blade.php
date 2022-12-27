6996ffc98e74487a8b7c0d230bc813aa12d5ab57
Arjuna Panji <arjuna.panji97@gmail.com>
2022-12-27 18:10:31 +0700

    fix scan result

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -1,4 +1,4 @@
-@if(Auth::check()) @extends('layouts.main') @section('content') @endif
+@if(Auth::check()) @extends('layouts.main') @section('content')
 <style>
     img {
         margin: auto;
@@ -44,4 +44,50 @@
     });
     html5QrcodeScanner.render(onScanSuccess, onScanError);
 </script>
-@if(Auth::check()) @endsection @endif
+@endsection @else
+<style>
+    img {
+        margin: auto;
+    }
+    #html5-qrcode-button-camera-permission,
+    #html5-qrcode-button-camera-start,
+    #html5-qrcode-button-camera-stop {
+        margin-top: 10px;
+        background-color: #570cad;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+    }
+    #html5-qrcode-anchor-scan-type-change {
+        visibility: hidden;
+    }
+</style>
+<div class="w-full h-full" id="reader"></div>
+
+<script src="{{ asset('js/html5-qrcode.min.js') }}"></script>
+<script>
+    function onScanSuccess(decodedText, decodedResult) {
+        // Handle on success condition with the decoded text or result.
+        // alert(`Scan result: ${decodedText}`, decodedResult);
+        window.location.replace(decodedText);
+    }
+
+    var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+        fps: 10,
+        qrbox: 250,
+    });
+
+    function onScanError(errorMessage) {
+        // handle on error condition, with error message
+    }
+
+    var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+        fps: 10,
+        qrbox: 250,
+    });
+    html5QrcodeScanner.render(onScanSuccess, onScanError);
+</script>
+@endif
